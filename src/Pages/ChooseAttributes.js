a05068a8966496d6d2e24b77dfd7eac602494ca3
Jeremy Troshynski <jtroshynski@gmail.com>
2018-08-17 18:49:59 -0500

    Added more functionality to attribute class

--- src/Pages/ChooseAttributes.js
+++ src/Pages/ChooseAttributes.js
@@ -1,29 +1,75 @@
 import React, { Component } from "react";
 import "Stylesheets/App.css";
+import axios from "axios";
+import Tile from "Components/Tile.js";
+import idiot from "Images/People/idiot.png";
+import biggestIdiot from "Images/People/biggestIdiot.png";
+import ericWilson from "Images/People/ericWilson.png";
 // import Attribute from "Components/Attribute";
 
 import { Analytics } from "aws-amplify";
 
 class ChooseAttributes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      attributes: [],
+      count: 0,
+      points: 0
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  attributeArray = [
+    { id: 0, attribute: idiot },
+    { id: 1, attribute: ericWilson },
+    { id: 2, attribute: idiot },
+    { id: 3, attribute: idiot },
+    { id: 4, attribute: idiot },
+    { id: 5, attribute: biggestIdiot }
+  ];
+
+  async componentDidMount() {
+    await axios.get("http://www.dnd5eapi.co/api/ability-scores/").then(res => {
+      const attributeData = res.data.results;
+      const count = res.data.count;
+      this.setState({ attributes: attributeData, count: count });
+    });
+  }
+
+  // Creates Tile components out of the raceArray and api data
+  AttributeTiles(props) {
+    const attributeTiles = props.attributeArray.map(attribute => (
+      <Tile
+        key={attribute.id}
+        image={attribute.image}
+        link="/chooseAttributes"
+        name={props.attributes[attribute.id].name}
+      />
+    ));
+    return <ul className="tiles-section">{attributeTiles}</ul>;
+  }
+
   render() {
     Analytics.record("appRender");
-    return (
-      <div className="content">
-        <h1 className="pageHeader">Pick attribute scores</h1>
-
-        <div> Your points, sucka </div>
-        <p> points = </p>
-
-        {/* <ul className="tiles-section"> */}
-        {/* <Attribute
-            id={"5"}
-            image={dragonborn}
-            link="/chooseClass"
-            text="Strength"
+    if (this.state.count === 0) {
+      return (
+        <div className="content">
+          <span>Loading...</span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="content">
+          <h1 className="pageHeader">Pick a attribute</h1>
+          <this.AttributeTiles
+            attributeArray={this.attributeArray}
+            {...this.state}
           />
-        </ul> */}
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
